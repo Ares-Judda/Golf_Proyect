f7b16cffd139e6608321134bd3fe6f8656c1eaf7
ARES JUDDA <ares.judda@gmail.com>
2024-11-12 17:10:22 -0600

    modificación del actualizar usuario, los datos del usuario se actualizan dependiendo su rol en su tabla asignada en la base de datos

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,10 +4,10 @@ const userTokenManager = require('../helpers/user-token-manager');
 const bcrypt = require('bcrypt');
 
 /**
- * Obtiene lista de todos los usuarios de la base de datos.
- * Este método consulta todos los usuarios almacenados y devuelve la lista en formato JSON.
- * @param {*} req - La solicitud HTTP (no se requieren parámetros específicos).
- * @param {*} res - La respuesta HTTP que contiene la lista de usuarios en formato JSON.
+ * Obtiene la lista de todos los usuarios de la base de datos.
+ * Consulta todos los registros en la tabla `user` y devuelve la lista en formato JSON.
+ * @param {*} req - La solicitud HTTP sin parámetros específicos.
+ * @param {*} res - La respuesta HTTP con la lista de usuarios en formato JSON.
  */
 const get_all_usuarios = async (req, res = response) => {
     try {
@@ -25,8 +25,9 @@ const get_all_usuarios = async (req, res = response) => {
 };
 
 /**
- * Guarda un usuario en la base de datos si el correo no existe.
- * @param {*} req - La solicitud HTTP con los campos obligatorios email, username y password en el cuerpo.
+ * Guarda un usuario en la base de datos si el correo y nombre de usuario no existen.
+ * Este método crea una entrada en la tabla `user` y asigna el rol en la tabla correspondiente (client o seller).
+ * @param {*} req - La solicitud HTTP con los campos obligatorios email, username, password, imagen, name y lastname en el cuerpo.
  * @param {*} res - La respuesta HTTP que contiene el resultado de la operación en formato JSON.
  */
 const save_usuario = async (req, res = response) => {
@@ -58,9 +59,10 @@ const save_usuario = async (req, res = response) => {
 
 /**
  * Obtiene un usuario por su ID.
- * Este método consulta la base de datos con un _id que se recibe en el cuerpo de la solicitud.
- * @param {*} req - La solicitud HTTP con el campo _id en el cuerpo.
- * @param {*} res - La respuesta HTTP que contiene el usuario encontrado o un mensaje de error.
+ * Consulta la base de datos usando el ID almacenado en `userTokenManager` y devuelve los datos completos del usuario.
+ * Incluye datos adicionales de la tabla `client` o `seller`, según el rol.
+ * @param {*} req - La solicitud HTTP, sin requerir ID en el cuerpo.
+ * @param {*} res - La respuesta HTTP con los datos del usuario encontrado o un mensaje de error.
  */
 const get_usuario = async (req, res = response) => {
     try {
@@ -68,12 +70,17 @@ const get_usuario = async (req, res = response) => {
         if (!idUser) {
             return res.status(400).json({ mensaje: 'Se requiere un ID' });
         }
+
         connection.query(
-            'SELECT * FROM user WHERE ID_User = ?',
+            `SELECT u.email, u.role, u.imagen, u.username, 
+                    c.name, c.lastname, c.cellphone, c.datebirth, c.address, c.zipcode
+             FROM golfdb.user AS u
+             INNER JOIN golfdb.client AS c ON u.ID_User = c.ID_User
+             WHERE u.ID_User = ?`,
             [idUser],
             (err, results) => {
                 if (err) {
-                    console.error('Error al buscar usuario por ID: ', err);
+                    console.error('Error al buscar usuario por ID:', err);
                     return res.status(500).json({ mensaje: 'Error interno del servidor' });
                 }
                 if (results.length === 0) {
@@ -83,17 +90,17 @@ const get_usuario = async (req, res = response) => {
             }
         );
     } catch (error) {
-        console.error('Error al buscar usuario por ID: ', error);
+        console.error('Error al buscar usuario por ID:', error);
         res.status(500).json({ mensaje: 'Error interno del servidor' });
     }
 };
 
+
 /**
- * Actualiza un usuario.
- * Este método recibe el _id y los campos a actualizar en el cuerpo de la solicitud,
- * realiza la actualización en la base de datos y devuelve el usuario actualizado.
- * @param {*} req - La solicitud HTTP con el campo _id y al menos un campo a actualizar en el cuerpo.
- * @param {*} res - La respuesta HTTP que contiene el usuario actualizado o un mensaje de error.
+ * Actualiza los datos de un usuario en las tablas `user`, `client` o `seller`.
+ * Divide los campos a actualizar entre la tabla `user` y la tabla específica (client o seller) según el rol.
+ * @param {*} req - La solicitud HTTP con los campos a actualizar en `actualizaciones`.
+ * @param {*} res - La respuesta HTTP que contiene el resultado de la operación o un mensaje de error.
  */
 const update_usuario_body = async (req, res = response) => {
     try {
@@ -105,22 +112,31 @@ const update_usuario_body = async (req, res = response) => {
         if (!actualizaciones || Object.keys(actualizaciones).length === 0) {
             return res.status(400).json({ mensaje: 'No se proporcionaron campos a actualizar' });
         }
-        const updates = Object.keys(actualizaciones).map(key => `${key} = ?`).join(', ');
-        const values = Object.values(actualizaciones);
-        connection.query(
-            `UPDATE user SET ${updates} WHERE ID_User = ?`,
-            [...values, idUser],
-            (err, result) => {
-                if (err) {
-                    console.error('Error al actualizar usuario:', err);
-                    return res.status(500).json({ mensaje: 'Error interno al actualizar usuario' });
-                }
-                if (result.affectedRows === 0) {
-                    return res.status(404).json({ mensaje: 'Usuario no encontrado' });
-                }
-                res.json({ mensaje: 'Usuario actualizado exitosamente' });
+        if (actualizaciones.username) {
+            const existUserName = await userNameExist(actualizaciones.username);
+            if (existUserName) {
+                return res.status(400).json({ error: 'El nombre de usuario ya está registrado' });
             }
-        );
+        }
+        const userFields = ['email', 'role', 'imagen', 'username'];
+        const userUpdates = {};
+        const roleSpecificUpdates = {};
+        Object.keys(actualizaciones).forEach(key => {
+            if (userFields.includes(key)) {
+                userUpdates[key] = actualizaciones[key];
+            } else {
+                roleSpecificUpdates[key] = actualizaciones[key];
+            }
+        });
+        if (Object.keys(userUpdates).length > 0) {
+            await updateTable('user', userUpdates, idUser);
+        }
+        const role = await getUserRole(idUser);
+        const tablaEspecifica = role === 'CLIENT_ROLE' ? 'client' : 'seller';
+        if (Object.keys(roleSpecificUpdates).length > 0) {
+            await updateTable(tablaEspecifica, roleSpecificUpdates, idUser);
+        }
+        res.json({ mensaje: 'Usuario actualizado exitosamente' });
     } catch (error) {
         console.error('Error en la actualización del usuario:', error);
         res.status(500).json({ mensaje: 'Error interno en el servidor' });
@@ -129,9 +145,9 @@ const update_usuario_body = async (req, res = response) => {
 
 /**
  * Cierra la sesión del usuario.
- * Este método elimina la referencia del usuario en el UserTokenManager y "mata" el token al cerrar sesión.
+ * Utiliza `userTokenManager` para eliminar la referencia del usuario actual y finalizar la sesión.
  * @param {*} req - La solicitud HTTP.
- * @param {*} res - La respuesta HTTP que contiene un mensaje de confirmación o de error.
+ * @param {*} res - La respuesta HTTP que contiene un mensaje de confirmación o error.
  */
 const logout_usuario = (req, res = response) => {
     try {
@@ -150,8 +166,10 @@ const logout_usuario = (req, res = response) => {
 
 /**
  * Inserta un nuevo usuario en la base de datos.
- * @param {Object} userData - Los datos del usuario a registrar.
- * @returns {Promise<string>} - Devuelve el ID del usuario recién registrado.
+ * Esta función realiza una consulta para insertar un nuevo usuario en la tabla `user` con un UUID generado automáticamente
+ * y devuelve el ID de usuario recién registrado.
+ * @param {Object} userData - Un objeto que contiene email, hashedPassword, role, imagen y userName del usuario.
+ * @returns {Promise<string>} - Devuelve el ID del usuario recién registrado o un error si la operación falla.
  */
 async function insertUser(userData) {
     const { email, hashedPassword, role, imagen, userName } = userData;
@@ -176,12 +194,28 @@ async function insertUser(userData) {
     });
 }
 
+/**
+ * Cifra la contraseña del usuario utilizando bcrypt.
+ * Esta función genera un hash seguro para almacenar la contraseña en la base de datos.
+ * @param {string} password - La contraseña en texto plano.
+ * @returns {Promise<string>} - Devuelve la contraseña cifrada.
+ */
 async function hashPassword(password) {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     return hashedPassword; 
 }
 
+/**
+ * Registra un usuario en la tabla correspondiente a su rol (cliente o vendedor).
+ * Inserta el usuario en la tabla `client` si es un cliente o en `selling` si es un vendedor.
+ * @param {string} email - El correo del usuario.
+ * @param {string} role - El rol del usuario (CLIENT_ROLE o SELLER_ROLE).
+ * @param {string} name - Nombre del usuario.
+ * @param {string} lastname - Apellido del usuario.
+ * @param {string} userId - ID del usuario en la tabla `user`.
+ * @returns {Promise<Object>} - Devuelve un objeto con `success: true` si la operación fue exitosa o un mensaje de error en caso de falla.
+ */
 async function registerTypeUser(email, role, name, lastname, userId) {
     return new Promise((resolve) => {
         const query = role === 'CLIENT_ROLE'
@@ -198,6 +232,11 @@ async function registerTypeUser(email, role, name, lastname, userId) {
     });
 }
 
+/**
+ * Verifica si el correo ya existe en la base de datos.
+ * @param {string} email - El correo del usuario a verificar.
+ * @returns {Promise<boolean>} - Devuelve `true` si el correo existe, o `false` si no existe.
+ */
 async function emailExist(email) {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM user WHERE email = ?', [email], (err, results) => {
@@ -210,6 +249,11 @@ async function emailExist(email) {
     });
 }
 
+/**
+ * Verifica si el nombre de usuario ya existe en la base de datos.
+ * @param {string} userName - El nombre de usuario a verificar.
+ * @returns {Promise<boolean>} - Devuelve `true` si el nombre de usuario existe, o `false` si no existe.
+ */
 async function userNameExist(userName) {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM user WHERE username = ?', [userName], (err, results) => {
@@ -219,6 +263,56 @@ async function userNameExist(userName) {
             }
             resolve(results.length > 0);
         });
+    });
+}
+
+/**
+ * Actualiza una tabla específica con los campos proporcionados para un usuario.
+ * @param {string} tabla - El nombre de la tabla a actualizar (`user`, `client`, o `seller`).
+ * @param {Object} campos - Un objeto donde las claves son los nombres de los campos a actualizar y los valores son los nuevos valores.
+ * @param {string} idUser - ID del usuario en la tabla `user`.
+ * @returns {Promise} - Una promesa que se resuelve si la actualización fue exitosa o se rechaza con un mensaje de error.
+ */
+async function updateTable(tabla, campos, idUser) {
+    return new Promise((resolve, reject) => {
+        const updates = Object.keys(campos).map(key => `${key} = ?`).join(', ');
+        const values = Object.values(campos);
+        connection.query(
+            `UPDATE ${tabla} SET ${updates} WHERE ID_User = ?`,
+            [...values, idUser],
+            (err, result) => {
+                if (err) {
+                    console.error(`Error al actualizar en ${tabla}:`, err);
+                    return reject(new Error('Error interno al actualizar usuario'));
+                }
+                resolve(result);
+            }
+        );
+    });
+}
+
+/**
+ * Obtiene el rol del usuario dado su ID.
+ * Consulta el rol en la tabla `user` utilizando el ID del usuario.
+ * @param {string} idUser - ID del usuario en la tabla `user`.
+ * @returns {Promise<string>} - Devuelve el rol del usuario o un error si el usuario no es encontrado.
+ */
+async function getUserRole(idUser) {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            'SELECT role FROM user WHERE ID_User = ?',
+            [idUser],
+            (err, results) => {
+                if (err) {
+                    console.error('Error al obtener rol del usuario:', err);
+                    return reject(new Error('Error al obtener rol del usuario'));
+                }
+                if (results.length === 0) {
+                    return reject(new Error('Usuario no encontrado'));
+                }
+                resolve(results[0].role);
+            }
+        );
     });
 }
 
