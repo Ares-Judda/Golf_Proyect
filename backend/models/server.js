119f2033f24d14d5842c2d73fc31604e082cd0df
ARES JUDDA <ares.judda@gmail.com>
2024-11-26 16:46:21 -0600

    Agregacion del yaml para la implementacion de swagger para el proyecto y pruebas

--- backend/models/server.js
+++ backend/models/server.js
@@ -2,29 +2,43 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connection = require('./database');
+const path = require('path');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
 class Server {
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+        this.swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
         this.middlewares();
         this.routes();
+        this.setupSwagger();
     }
 
     middlewares() {
-        this.app.use(cors());
+        this.app.use(cors({
+            origin: '*', // Permite cualquier origen
+            methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+            allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+        }));
         this.app.use(express.json());  
         this.app.use(express.static('public'));
-    }    
+    }   
 
     routes() {
         this.app.use('/api/usuarios', require('../routes/user'));  
         this.app.use('/api/auth', require('../routes/auth'));     
     }
 
+    setupSwagger() {
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(this.swaggerDocument));
+    }
+
     listen (){
         this.app.listen(this.port, () =>{
             console.log(`Server listening on port ${this.port}`)
+            console.log(`Documentación Swagger disponible en http://localhost:${this.port}/api-docs`);
         });
     }
 }
