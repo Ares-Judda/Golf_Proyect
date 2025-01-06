662e56291c57b3421dcccb4fa605476997267210
LuisElizaldeX <king_mbea@hotmail.com>
2025-01-06 03:15:13 -0600

    Recuperacion y agregacion de imagenes
    Agregacion de metodos para recuperar y guardar las imagenes de clothes

--- backend/services/routesRest/images.js
+++ backend/services/routesRest/images.js
@@ -4,5 +4,6 @@ const imagesController = require('../../Logic/controllersRest/images');
 const upload = require('../../business/helpers/multerConfig');
 
 router.post('/upload_image', upload.single('profileImage'), imagesController.uploadImage);
+router.delete('/delete_image/:filename', imagesController.deleteImage);
 
 module.exports = router;
