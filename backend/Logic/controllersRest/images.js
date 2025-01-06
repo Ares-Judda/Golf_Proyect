b97e273b3903709911035756222a8414efe9787d
ARES JUDDA <ares.judda@gmail.com>
2025-01-06 16:52:04 -0600

    cambios durante la revison del proyecto

--- backend/Logic/controllersRest/images.js
+++ backend/Logic/controllersRest/images.js
@@ -8,7 +8,7 @@ const uploadImage = (req, res) => {
         return res.status(400).send({ message: 'No se ha subido ningún archivo.' });
     }
     console.log(`Archivo guardado en: ${req.file.path}`); // Ruta física del archivo
-    const imageUrl = `http://localhost:${process.env.PORT || 8085}/uploads/${req.file.filename}`;
+    const imageUrl = `http://172.20.10.2:${process.env.PORT || 8085}/uploads/${req.file.filename}`;
     res.status(200).send({ url: imageUrl });
 };
 
